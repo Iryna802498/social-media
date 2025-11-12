a5d3e3a38add76990d9351540f14f7ceeda98519
Iryna Mishchenko <djrobentab@gmail.com>
2025-11-12 13:46:02 +0100

    Add Profile model

--- social/models.py
+++ social/models.py
@@ -1,3 +1,31 @@
+import os
+import uuid
 from django.db import models
+from social_media import settings
 
-# Create your models here.
+
+def user_profile_image_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    user_id = instance.user.id if hasattr(instance, "user") else instance.id
+    filename = f"user-id-{user_id}-{uuid.uuid4()}{extension}"
+    return os.path.join("uploads/users/", filename)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        related_name="profile",
+        on_delete=models.CASCADE
+    )
+    avatar = models.ImageField(
+        blank=True,
+        null=True,
+        upload_to=user_profile_image_path
+    )
+    bio = models.TextField(
+        blank=True,
+        null=True
+    )
+
+    def __str__(self) -> str:
+        return self.user.email
