5e21f5662a91d9b7bc1d85e50de2c30e4f918e47
Iryna Mishchenko <djrobentab@gmail.com>
2025-11-13 19:23:44 +0100

    Configure project URLs and API documentation routes

--- social_media/urls.py
+++ social_media/urls.py
@@ -16,8 +16,31 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from debug_toolbar.toolbar import debug_toolbar_urls
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+    path("api/social-media/", include("social.urls", namespace="social")),
+    path("api/user/", include("user.urls", namespace="user")),
+    path("api/doc/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui"
+    ),
+    path(
+        "api/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc"
+    ),
+    path("__debug__/", include("debug_toolbar.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
